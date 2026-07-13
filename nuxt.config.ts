ae15b970f67a94f6108b80e19d2eba86a9002648
Maksim Chipurnov <46316981+1Magz1@users.noreply.github.com>
2026-07-13 17:23:21 +0700

    main: add ag-grid plugin

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,14 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+  css: [
+    'ag-grid-community/styles/ag-grid.css',
+    'ag-grid-community/styles/ag-theme-quartz.css',
+  ],
+  vite: {
+    optimizeDeps: {
+      include: ['ag-grid-vue3', 'ag-grid-enterprise'],
+    },
+  },
 })
