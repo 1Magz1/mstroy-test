a832ec9c519f8b167fe8184579ebbc02d8c6753c
Maksim Chipurnov <46316981+1Magz1@users.noreply.github.com>
2026-07-14 12:50:59 +0700

    main: update layout of app

--- app/app.vue
+++ app/app.vue
@@ -1,6 +1,13 @@
 <template>
-  <div>
+  <div class="app">
     <NuxtRouteAnnouncer />
     <NuxtPage />
   </div>
 </template>
+
+<style lang="scss">
+.app {
+  min-height: 100dvh;
+  background-color: $color-bg-layout;
+}
+</style>
