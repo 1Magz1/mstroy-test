e1bd91c694b4a96d0f68ede5ab72735e5ae485d9
Maksim Chipurnov <46316981+1Magz1@users.noreply.github.com>
2026-07-14 12:37:50 +0700

    main: create useTreeStore composable

---
+++ app/composables/useTreeStore.ts
@@ -0,0 +1,51 @@
+import { TreeStore } from '~/shared/lib/TreeStore'
+import type { TreeItem, TreeItemId } from '~/shared/types/tree'
+
+export function useTreeStore<T extends TreeItem>(initialItems: T[]) {
+  const store = new TreeStore(initialItems)
+
+  function cloneItems() {
+    return store.getAll().map(item => ({ ...item }))
+  }
+
+  const items = shallowRef(cloneItems())
+
+  function sync() {
+    items.value = cloneItems()
+  }
+
+  const treeRows = computed(() => {
+    const _ = items.value.length
+    return store.getTreeRows()
+  })
+
+  function addItem(item: T) {
+    store.addItem(item)
+    sync()
+  }
+
+  function updateItem(item: T) {
+    store.updateItem(item)
+    sync()
+  }
+
+  function removeItem(id: T['id']) {
+    store.removeItem(id)
+    sync()
+  }
+
+  function getAllParents(id: TreeItemId) {
+    const _ = items.value.length
+    return store.getAllParents(id)
+  }
+
+  return {
+    store,
+    items,
+    treeRows,
+    addItem,
+    updateItem,
+    removeItem,
+    getAllParents,
+  }
+}
